411c1977a03caddc0586673f1a0c7ec27d4b2fe9
wenhe <zhentaogo@gmail.com>
2025-06-13 17:35:05 +0800

    feat: 添加cnm -v

--- bin.js
+++ bin.js
@@ -1,32 +1,39 @@
 #!/usr/bin/env node
 
-const path = require('path')
-const config = require('./config.json')
-const clearDir = require('./src/lib')
+const path = require("path");
+const config = require("./config.json");
+const clearDir = require("./src/lib");
 
-const argv = process.argv.slice(2)
+const { version } = require("../package.json");
 
-const log = console.log
+const args = process.argv.slice(2);
 
-const DIST_DIR = path.resolve(__dirname, argv[0] || config.dist_dir_path)
-const LIMIT_SIZE = argv[1] || config.dist_dir_size
-const NODE_MODULES = argv[2] || config.dist_dir_name
-
-let isHelp = false
-
-argv.forEach(arg => {
-  if (arg.match(/^(-+|\/)(h(elp)?|\?)$/)) isHelp = true
-})
-
-if (argv.length < 1) isHelp = true
-
-if (isHelp) {
-  log('')
-  log('useage: cnm <path> <limit(mb)> <distDir>')
-  log('')
-  log('  delete the specified folder!')
-  log('')
-  process.exit(1)
+if (args.includes("-v") || args.includes("--version")) {
+  console.log(`cnm v${version}`);
+  process.exit(0);
 }
 
-clearDir(DIST_DIR, LIMIT_SIZE, NODE_MODULES)
+const log = console.log;
+
+const DIST_DIR = path.resolve(__dirname, argv[0] || config.dist_dir_path);
+const LIMIT_SIZE = argv[1] || config.dist_dir_size;
+const NODE_MODULES = argv[2] || config.dist_dir_name;
+
+let isHelp = false;
+
+argv.forEach((arg) => {
+  if (arg.match(/^(-+|\/)(h(elp)?|\?)$/)) isHelp = true;
+});
+
+if (argv.length < 1) isHelp = true;
+
+if (isHelp) {
+  log("");
+  log("useage: cnm <path> <limit(mb)> <distDir>");
+  log("");
+  log("  delete the specified folder!");
+  log("");
+  process.exit(1);
+}
+
+clearDir(DIST_DIR, LIMIT_SIZE, NODE_MODULES);
